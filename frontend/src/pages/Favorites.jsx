85e209d300363858d5f76632a7d822083f0ee79b
Piyush Patil <piyushpatil1741@gmail.com>
2025-08-04 17:11:55 +0530

    Added Favorites Page and Router

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,7 @@
+function Favorites(){
+    return <div className="favorites_empty">
+        <h2> No Favorite Movies yet.</h2>
+        <p>Start adding movies to your Favorites and they will appear here</p>
+    </div>
+}
+export default Favorites 
